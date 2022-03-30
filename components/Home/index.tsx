cea04490e9fd114fdb90733ab29a17eddc99be48
goingdust <dustinluck@gmail.com>
2022-03-30 18:50:18 -0400

    Bring in AnimatedLetters to Home component

--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -2,19 +2,51 @@ import styles from './index.module.scss';
 import Image from 'next/image';
 import LogoTitle from '../../assets/images/logo-s.png';
 import Link from 'next/link';
+import { useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters';
 
 const Home = () => {
+	const [letterClass, setLetterClass] = useState('text-animate');
+	const nameArray = ['d', 'u', 's', 't', 'y', ' ', 'l', 'u', 'c', 'k'];
+	const jobArray = [
+		'w',
+		'e',
+		'b',
+		' ',
+		'd',
+		'e',
+		'v',
+		'e',
+		'l',
+		'o',
+		'p',
+		'e',
+		'r',
+	];
+
 	return (
 		<div className={`container ${styles.homePage}`}>
 			<div className={styles.textZone}>
 				<h1>
-					Hi, <br /> I&apos;m
+					<span className={letterClass}>H</span>
+					<span className={`${letterClass} _12`}>i,</span>
+					<br />
+					<span className={`${letterClass} _13`}>I</span>
+					<span className={`${letterClass} _14`}>&apos;m</span>
 					<div className={styles.bigLetterS}>
 						<Image src={LogoTitle} alt='developer' />
 					</div>
-					lobodan
+					<AnimatedLetters
+						letterClass={letterClass}
+						strArray={nameArray}
+						index={15}
+					/>
 					<br />
-					web developer
+					<AnimatedLetters
+						letterClass={letterClass}
+						strArray={jobArray}
+						index={25}
+					/>
 				</h1>
 				<h2>full stack developer</h2>
 				<Link href='/contact'>
