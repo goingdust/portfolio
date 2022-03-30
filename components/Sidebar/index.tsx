88e9425505f1023b8d2bf2c84ead6bb1f6903efc
goingdust <dustinluck@gmail.com>
2022-03-30 14:39:27 -0400

    Create 3 nav links

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -1,10 +1,15 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import Image from 'next/image';
 import styles from './index.module.scss';
 import LogoS from '../../assets/images/logo-s.png';
 import LogoSubtitle from '../../assets/images/logo_sub.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const Sidebar = () => {
+	const router = useRouter();
+
 	return (
 		<section className={styles.navBar}>
 			<Link href='/' passHref>
@@ -17,6 +22,42 @@ const Sidebar = () => {
 					</div>
 				</a>
 			</Link>
+			<nav>
+				<ul>
+					<li className={router.pathname === '/' ? 'active' : ''}>
+						<Link href='/' passHref>
+							<a>
+								<FontAwesomeIcon icon={faHome} color='#4d4d4'></FontAwesomeIcon>
+							</a>
+						</Link>
+					</li>
+					<li
+						className={`${styles.aboutLink} ${
+							router.pathname === '/about' ? 'active' : ''
+						}`}
+					>
+						<Link href='/about' passHref>
+							<a>
+								<FontAwesomeIcon icon={faUser} color='#4d4d4'></FontAwesomeIcon>
+							</a>
+						</Link>
+					</li>
+					<li
+						className={`${styles.contactLink} ${
+							router.pathname === '/contact' ? 'active' : ''
+						}`}
+					>
+						<Link href='/contact' passHref>
+							<a>
+								<FontAwesomeIcon
+									icon={faEnvelope}
+									color='#4d4d4'
+								></FontAwesomeIcon>
+							</a>
+						</Link>
+					</li>
+				</ul>
+			</nav>
 		</section>
 	);
 };
