58dd85b2d0d69f5496a9d92a210a4c0a2a25574c
goingdust <dustinluck@gmail.com>
2022-03-30 13:44:51 -0400

    Add example logos to assets folder and import one

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -1,7 +1,19 @@
+import Link from 'next/link';
+import Image from 'next/image';
 import styles from './index.module.scss';
+import LogoS from '../../assets/images/logo-s.png';
 
 const Sidebar = () => {
-	return <>I am the Sidebar!</>;
+	return (
+		<div className='nav-bar'>
+			<Link href='/' passHref>
+				<div className='logo'>
+					<Image src={LogoS} alt='logo' />
+					<Image src={LogoS} alt='logo' />
+				</div>
+			</Link>
+		</div>
+	);
 };
 
 export default Sidebar;
