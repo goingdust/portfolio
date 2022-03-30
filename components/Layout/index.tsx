2b8aff31b5689b1a8d9131f08902b4da283e4060
goingdust <dustinluck@gmail.com>
2022-03-30 17:34:22 -0400

    Add visual body tags to the page and style layout component

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -3,9 +3,17 @@ import styles from './index.module.scss';
 
 const Layout = () => {
 	return (
-		<>
+		<div className={styles.App}>
 			<Sidebar />
-		</>
+			<div className={styles.page}>
+				<span className={`${styles.tags} ${styles.topTags}`}>&lt;body&gt;</span>
+				<span className={`${styles.tags} ${styles.bottomTags}`}>
+					&lt;body&gt;
+					<br />
+					<span className={styles.bottomTagHtml}>&lt;/html&gt;</span>
+				</span>
+			</div>
+		</div>
 	);
 };
 
