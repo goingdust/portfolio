2538d3b1adc986b5af83f829698e64b292864dda
goingdust <dustinluck@gmail.com>
2022-03-30 14:21:32 -0400

    Insert logo for nav bar and style it

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -2,17 +2,22 @@ import Link from 'next/link';
 import Image from 'next/image';
 import styles from './index.module.scss';
 import LogoS from '../../assets/images/logo-s.png';
+import LogoSubtitle from '../../assets/images/logo_sub.png';
 
 const Sidebar = () => {
 	return (
-		<div className='nav-bar'>
+		<section className={styles.navBar}>
 			<Link href='/' passHref>
-				<div className='logo'>
-					<Image src={LogoS} alt='logo' />
-					<Image src={LogoS} alt='logo' />
-				</div>
+				<a className={styles.logoContainer}>
+					<div className={styles.logos}>
+						<Image src={LogoS} alt='logo' />
+					</div>
+					<div className={`${styles.logos} ${styles.subLogo}`}>
+						<Image src={LogoSubtitle} alt='slobodan' />
+					</div>
+				</a>
 			</Link>
-		</div>
+		</section>
 	);
 };
 
