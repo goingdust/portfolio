3a6549df3729a1dedb97309fbdd4f993403eec4f
goingdust <dustinluck@gmail.com>
2022-11-08 16:06:00 -0500

    Remove button text and animate arrow

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -8,17 +8,30 @@ const Contact = () => {
 	return (
 		<div className={styles.contactPage}>
 			<form action='https://api.web3forms.com/submit' method='POST'>
+				<h1>Drop me a line.</h1>
 				<input type='hidden' name='access_key' value={process.env.WEB3_FORMS_ACCESS_KEY} />
 				<input type='checkbox' name='botcheck' className='hidden' style={{ display: 'none' }} />
 
-				<Input contactStyles={styles} name='name' type='text' id='name' placeholder='Your name' />
-				<Input contactStyles={styles} name='email' type='text' id='email' placeholder='Email' />
-				<Textarea contactStyles={styles} name='message' id='message' placeholder='Pour your heart out!' />
+				<Input
+					contactStyles={styles}
+					name='name'
+					type='text'
+					id='name'
+					placeholder='-> Your name'
+				/>
+				<Input contactStyles={styles} name='email' type='text' id='email' placeholder='-> Email' />
+				<Textarea
+					contactStyles={styles}
+					name='message'
+					id='message'
+					placeholder='-> Pour your heart out!'
+				/>
 
 				<input type='hidden' name='redirect' value='https://web3forms.com/success' />
 
-				<button type='submit' disabled>
-					Send <Image src={Mail} alt='mail' />
+				<button type='submit' disabled aria-label='Send Message'>
+					<Image src={Mail} alt='mail' />
+					<span>{'->'}</span>
 				</button>
 			</form>
 		</div>
