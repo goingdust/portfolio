9e52efc43af26d888e2cffd6aa10cf0b43dab7c4
goingdust <dustinluck@gmail.com>
2022-11-17 19:56:04 -0500

    Add bio

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -1,9 +1,15 @@
-import styles from '../index.module.scss';
+import styles from './index.module.scss';
 
 const About = () => {
 	return (
-		<div>
+		<div className={styles.container}>
 			<h1>A Lil Bit About Me</h1>
+			<p className={styles.bio}>
+				{`I started dabbling in web dev in early 2021 after building my PC. What was a little hobby
+				soon became a career change and pushed me to enroll in Lighthouse Labs' Web Development
+				Bootcamp. Coding's allure of seemingly-endless building possibilities keeps me learning and
+				working to realize all the ideas percolating around my head.`}
+			</p>
 		</div>
 	);
 };
